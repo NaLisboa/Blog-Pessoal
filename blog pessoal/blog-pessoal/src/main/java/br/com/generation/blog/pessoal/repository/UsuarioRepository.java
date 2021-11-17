5314aa2ec69a6c399260f8f7a21768539805d488
NaLisboa <nathalialisboa01@gmail.com>
2021-11-16 23:15:43 -0300

    Atualizando com testes JUnit

--- blog pessoal/blog-pessoal/src/main/java/br/com/generation/blog/pessoal/repository/UsuarioRepository.java
+++ blog pessoal/blog-pessoal/src/main/java/br/com/generation/blog/pessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.generation.blog.pessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,7 @@ import br.com.generation.blog.pessoal.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 
 }
